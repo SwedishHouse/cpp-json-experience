1346926479bc230e3c9d08965d55185e1389240a
SwedishHouse <swedish2015@yandex.ru>
2025-01-18 12:01:36 +0400

    git update

---
+++ build/CMakeFiles/CppJsonExperience.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppJsonExperience.dir/main.cpp.o"
+  "CMakeFiles/CppJsonExperience.dir/main.cpp.o.d"
+  "CppJsonExperience"
+  "CppJsonExperience.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppJsonExperience.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CppJsonExperience.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CppJsonExperience.
+# This may be replaced when dependencies are built.
